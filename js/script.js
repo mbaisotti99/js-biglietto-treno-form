c6c47f46cee7802e8dd37b1d48d731839d5bffd5
mbaisotti99 <matteo.baisotti@gmail.com>
2024-11-11 10:26:36 +0100

    feat:finish

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ let psngrKms = document.getElementById("kms");
 
 let psngrAge = document.getElementById("age");
 
-let sub = document.getElementById("send");
+let sub = document.getElementById("sendIt");
 
 let clear = document.getElementById("clear");
 
 
-
+let tktCard = document.getElementById("ticket");
 let tktName = document.getElementById("passName");
 let tktOff = document.getElementById("passOff");
 let tktClass = document.getElementById("passClass");
@@ -19,39 +19,56 @@ let tktCost = document.getElementById("passCost");
 
 
 sub.addEventListener("submit", function(event) {
-    event.preventDefault();
+    event.preventDefault(); //Non va
     
     let minorenne = false;
     let over = false;
     
     // NOME 
     
-    tktName.innerHTML = psngrName;
+    tktName.innerHTML = psngrName.value.trim();
     
     // ETA'
     
-    tktOff.innerHTML = () => {
-        if (parseInt(psngrAge) < 18){
-            minorenne = true;
-            return "-20%"
-        } else if (parseInt(psngrAge) > 65){
-            over = true;
-            return "-40%"
-        } else {
-            return "Standard"
-        }
+    let offer = "Standard";
+
+    if (psngrAge.value == "minor"){
+        minorenne = true;
+        offer = "Minorenne"
+    } else if (psngrAge.value == "over"){
+        over = true;
+        offer  = "-40%"
     }
+
+    tktOff.innerHTML =  offer;
+
+    // Classe     
     
-    tktClass.innerHTML = () => Math.floor(Math.random() * 3 + 1)
+    let passClass = Math.floor(Math.random() * 3 + 1);
+
+    tktClass.innerHTML = passClass + "°";
+
+    // Codice CP 
     
-    
-    
+    let cpCode = "#" + Math.floor(Math.random() * 99999 + 1);
+
+    tktCp.innerHTML = cpCode;
+
+    let cost = (parseInt(psngrKms.value.trim()) * 0.21);
+    if (minorenne){
+        cost -= (cost/100)*20  
+    } else if (over){
+        cost -= (cost/100)*40
+    } 
+    tktCost.innerHTML = cost.toFixed(2) + "€";
+        
     
 
+    tktCard.classList.remove("d-none");
 })
 
 clear.addEventListener("click", function(event) {
-    event.preventDefault;
+    event.preventDefault();
 
     form.reset;
 })
